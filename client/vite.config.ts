2e372a8b2c1a4523288367ecdd8773402a68e5be
Cristian Londoño <98239389+CrisD3v@users.noreply.github.com>
2022-11-17 20:13:03 -0500

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,16 +4,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'client/src/page/Landing/Landing.tsx'),
-        home: resolve(__dirname, 'client/src/page/Home/Home.tsx'),
-        about: resolve(__dirname, 'client/src/page/About/About.tsx'),
-        dashboard: resolve(__dirname, 'client/src/page/Dashboard/Dashboard.tsx'),
-        dashadmin: resolve(__dirname, 'client/src/page/Dashboard/DAdmin.tsx'),
-      }
-    }
-  }
+  plugins: [react()]
 })

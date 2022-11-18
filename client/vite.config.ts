c2de064e8b2ba7b90834d868aff64a1f5f52d66a
CrisD3v <crisdevenginer@gmail.com>
2022-11-18 09:57:12 -0500

    Push Route

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    pages({
+      dirs: [
+        {dir: 'src/page/Landing', baseRoute: ''},
+        {dir: 'src/page/Home', baseRoute: 'Home'},
+        {dir: 'src/page/Dashboard', baseRoute: 'Dashboard'},
+        {dir: 'src/page/Dashboard', baseRoute: 'DAdmin'},
+        {dir: 'src/page/About', baseRoute: 'About'},
+      ],
+      extensions: ['tsx,ts'],
+      exclude: ['**/components/**/*'],
+    })
+  ]
 })
